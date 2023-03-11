83ffafffcb4a113138bb42251cc2e3a59738f363
Daniel Friyia <dfriyia@gmail.com>
2023-03-11 16:27:50 -0500

    Android Works

--- android/src/main/java/expo/modules/samplegyroview/ExpoSampleGyroView.kt
+++ android/src/main/java/expo/modules/samplegyroview/ExpoSampleGyroView.kt
@@ -1,7 +1,29 @@
 package expo.modules.samplegyroview
 
 import android.content.Context
+import android.graphics.Color
+import android.util.TypedValue
+import android.view.Gravity
+import android.widget.TextView
 import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.viewevent.EventDispatcher
 import expo.modules.kotlin.views.ExpoView
 
-class ExpoSampleGyroView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
+class ExpoSampleGyroView(context: Context, appContext: AppContext) : ExpoView(context, appContext) {
+    val onGyroEvent by EventDispatcher()
+
+    internal val textView = TextView(context).also {
+        addView(it)
+        it.text = "START"
+        it.layoutParams = LayoutParams(
+            android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+            android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+        )
+        it.gravity = Gravity.CENTER
+        it.setTextSize(TypedValue.COMPLEX_UNIT_SP,70f);
+    }
+
+    fun updateText(newValue: String) {
+        textView.text = newValue
+    }
+}
--- android/src/main/java/expo/modules/samplegyroview/ExpoSampleGyroViewModule.kt
+++ android/src/main/java/expo/modules/samplegyroview/ExpoSampleGyroViewModule.kt
@@ -1,46 +1,75 @@
 package expo.modules.samplegyroview
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import androidx.core.os.bundleOf
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
+import expo.modules.kotlin.viewevent.EventDispatcher
+
+const val kOnGyroEvent = "onGyroEvent"
+
+// Currently Screams about BlastBufferQueue on Android 13.
+// It seems this warning can be safely ignored.
+// Spams the logs really badly though
+// See: https://stackoverflow.com/a/75041612
+class StepEventListener(
+  private val cb: (newValue: String) -> Unit
+): SensorEventListener {
+  var isTracking: Boolean = true
+
+  override fun onSensorChanged(event: SensorEvent?) {
+    if(isTracking && event != null) {
+      val rawY = -event.values[1]
+      if(event.timestamp % 5 == 0L) {
+        cb(String.format("%.2f", rawY))
+      }
+    }
+  }
+  override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
+}
 
 class ExpoSampleGyroViewModule : Module() {
-  // Each module class must implement the definition function. The definition consists of components
-  // that describes the module's functionality and behavior.
-  // See https://docs.expo.dev/modules/module-api for more details about available components.
+  var sensorManager: SensorManager? = null
+  var stepListener: StepEventListener? = null
+  var isInitialized: Boolean = false
+  var placeholderText: String? = null
+
   override fun definition() = ModuleDefinition {
-    // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
-    // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
-    // The module will be accessible from `requireNativeModule('ExpoSampleGyroView')` in JavaScript.
     Name("ExpoSampleGyroView")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
-    }
-
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
     View(ExpoSampleGyroView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: ExpoSampleGyroView, prop: String ->
-        println(prop)
+      Events(kOnGyroEvent)
+
+      Prop("placeholderText") { view: ExpoSampleGyroView, text: String? ->
+        view.textView.text = text ?: "START"
+        placeholderText = text
+      }
+
+      Prop("track") { view: ExpoSampleGyroView, isTracking: Boolean ->
+        val activity = appContext.activityProvider?.currentActivity
+        val applicationContext = activity?.applicationContext
+
+        if(applicationContext != null && !isInitialized) {
+          isInitialized = true
+          sensorManager =
+            applicationContext.applicationContext.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+          stepListener = StepEventListener {
+              view.updateText(it)
+              view.onGyroEvent(mapOf("y" to it))
+          }
+
+          val stepSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+          sensorManager?.registerListener(stepListener, stepSensor, SensorManager.SENSOR_STATUS_ACCURACY_HIGH)
+        }
+
+        stepListener?.isTracking = isTracking
+        view.textView.text = placeholderText ?: "START"
       }
     }
   }
